b9d7861b89069ee98583a2bd5322b8312bf1dd17
Hanzla Nouman <64569364+HanzlaNouman@users.noreply.github.com>
2024-02-25 22:23:26 +0500

    UI Fix

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import TodoList from './components/TodoList';
 
 function App() {
   return (
-    <div className="flex flex-col items-center bg-zinc-900 h-[100vh]">
+    <div className="flex flex-col items-center bg-zinc-900 min-h-[100vh]">
       <TodoForm />
       <TodoList />
       <Toaster position='bottom-center' />
--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
@@ -6,20 +6,22 @@ import toast from 'react-hot-toast';
 import { Input } from './ui/input';
 import { Button } from './ui/button';
 import { useTodoStore } from '../store';
+import { useEffect } from 'react'; s
 
-// Validation Schema for the Todo Form 
+// Validation Schema for the Todo Form
 const schema = z.object({
     text: z.string().min(3, 'Todo must be at least 3 characters long'),
 });
-// FormData type for the Todo Form
 
+// FormData type for the Todo Form
 type FormData = {
     text: string;
 };
 
 const TodoForm: React.FC = () => {
+    const [submitted, setSubmitted] = useState(false); // State to track form submission
 
-    // React Hook Form 
+    // React Hook Form
     const {
         register,
         handleSubmit,
@@ -30,23 +32,22 @@ const TodoForm: React.FC = () => {
     });
 
     // Zustand Store
-
     const addTodo = useTodoStore((state) => state.addTodo);
 
     // Form Submit Handler
-
     const onSubmit = (data: FormData) => {
         addTodo(data.text);
         reset();
+        setSubmitted(true); // Set submitted to true on form submit
         toast.success('Todo added successfully!');
     };
 
     // Error Handling for the Todo Form with Toast
     useEffect(() => {
-        if (errors.text?.message) {
+        if (submitted && errors.text?.message) {
             toast.error(errors.text.message);
         }
-    }, [errors.text]);
+    }, [errors.text, submitted]);
 
     return (
         <form
@@ -59,12 +60,10 @@ const TodoForm: React.FC = () => {
                     placeholder='Start Working on a new task...'
                     className='bg-zinc-800 w-full text-white placeholder-gray-400 focus:ring-0 focus:border-0 focus:outline-none'
                 />
-
             </div>
             <Button type='submit' className='p-4 px-6 bg-green-500'>
                 Add
             </Button>
-
         </form>
     );
 };
